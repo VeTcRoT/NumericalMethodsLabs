dcec26a5e10a8affc4a5e5eae12c16c6a8db73c1
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-03-12 19:53:09 +0200

    Adding delegates to implement versatility.

--- Lab_2/Lab_2/Delegates.cs
+++ Lab_2/Lab_2/Delegates.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lab_2
 {
-    internal class Delegates
-    {
-    }
+    public delegate double Function(double operand);
+    public delegate double FirstDerivativeOfFunction(double operand);
+    public delegate double SecondDerivativeOfFunction(double operand);
+    public delegate double IterationTypeFunction(double operand);
+    public delegate double FirstDerivativeOfIterationTypeFunction(double operand);
 }
